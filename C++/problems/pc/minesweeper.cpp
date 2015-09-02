e6409c732136a855ed10ade1b9eb78e2531eeb49
mythr <oscar.o.silver@gmail.com>
2015-09-02 10:42:39 +0200

    Added solution for UVA Minesweeper problem

---
+++ C++/problems/pc/minesweeper.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 200;
+const int MIDDLE = 100;
+
+char input[MAX][MAX];
+int output[MAX][MAX];
+
+int main(){
+	int rows, cols;
+	static int runs = 1;
+	while(cin >> rows >> cols){
+		if(rows == 0 && cols == 0)
+			break;
+		for(int i = MIDDLE; i < rows + MIDDLE; i++){
+			for(int j = MIDDLE; j < cols + MIDDLE; j++){
+				char tmp;
+				cin >> tmp;
+				input[i][j] = tmp;
+				output[i][j] = 0;
+			}
+		}
+		for(int i = MIDDLE; i < rows + MIDDLE; i++){
+			for(int j = MIDDLE; j < cols + MIDDLE; j++){
+				if(input[i][j] == '*')
+				{
+					output[i][j] = '*';
+					output[i][j+1] += 1;
+					output[i+1][j+1] += 1;
+					output[i-1][j+1] += 1;
+					output[i][j-1] += 1;
+					output[i+1][j-1] += 1;
+					output[i-1][j-1] += 1;
+					output[i+1][j] += 1;
+					output[i-1][j] += 1;
+
+				}
+			}
+		}
+		cout << "Field #" << runs <<":" << endl;
+		for(int i = MIDDLE; i < rows + MIDDLE; i++){
+			for(int j = MIDDLE; j < cols + MIDDLE; j++){
+				if(input[i][j] == '*')
+					cout << '*';
+				else
+					cout << output[i][j];
+			}
+			cout << endl;
+		}
+	}
+	
+
+	return 0;
+}
